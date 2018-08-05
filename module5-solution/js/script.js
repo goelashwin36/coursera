aba17f622a56db82917c126ad2b47fc3bb254482
Ashwin Goel <ashwin.goel2017@vitstudent.ac.in>
2018-08-05 11:49:46 +0530

    http changed to https

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -15,11 +15,11 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
     var homeHtmlUrl = "snippets/home-snippet.html";
     var allCategoriesUrl =
-        "http://davids-restaurant.herokuapp.com/categories.json";
+        "https://davids-restaurant.herokuapp.com/categories.json";
     var categoriesTitleHtml = "snippets/categories-title-snippet.html";
     var categoryHtml = "snippets/category-snippet.html";
     var menuItemsUrl =
-        "http://davids-restaurant.herokuapp.com/menu_items.json?category=";
+        "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
     var menuItemsTitleHtml = "snippets/menu-items-title.html";
     var menuItemHtml = "snippets/menu-item.html";
 
@@ -43,7 +43,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
         string = string
             .replace(new RegExp(propToReplace, "g"), propValue);
         return string;
-    }
+    };
 
 // Remove the class 'active' from home and switch to Menu button
     var switchMenuToActive = function () {
@@ -54,7 +54,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
         // Add 'active' to menu button if not already there
         classes = document.querySelector("#navMenuButton").className;
-        if (classes.indexOf("active") == -1) {
+        if (classes.indexOf("active") === -1) {
             classes += " active";
             document.querySelector("#navMenuButton").className = classes;
         }
@@ -82,12 +82,10 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 // On first load, show home view
         showLoading("#main-content");
         $ajaxUtils.sendGetRequest(
-            allCategoriesUrl,
-            function (categories) {
-                buildAndShowHomeHTML(categories);
-            },
+            allCategoriesUrl, buildAndShowHomeHTML,  // ***** <---- TODO: STEP 1: Substitute [...] ******
             true); // Explicitely setting the flag to get JSON from server processed into an object literal
     });
+
 // *** finish **
 
 
@@ -103,7 +101,8 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
                 // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
                 // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
                 // variable's name implies it expects.
-                var chosenCategoryShortName = "'" + chooseRandomCategory(categories).short_name + "'";
+                var chosenCategoryShortName = chooseRandomCategory (categories).short_name;
+
 
                 // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
                 // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -115,11 +114,14 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
                 // $dc.loadMenuItems('L')
                 // Hint: you need to surround the chosen category short name with something before inserting
                 // it into the home html snippet.
-                var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, 'randomCategoryShortName', chosenCategoryShortName)
+                //
+                var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
+
 
                 // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
                 // Use the existing insertHtml function for that purpose. Look through this code for an example
                 // of how to do that.
+                // ....
                 insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
             },
             false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
@@ -293,7 +295,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
                     menuItems[i].description);
 
             // Add clearfix after every second menu item
-            if (i % 2 != 0) {
+            if (i % 2 !== 0) {
                 html +=
                     "<div class='clearfix visible-lg-block visible-md-block'></div>";
             }
@@ -312,7 +314,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
                              priceValue) {
         // If not specified, replace with empty string
         if (!priceValue) {
-            return insertProperty(html, pricePropName, "");;
+            return insertProperty(html, pricePropName, "");
         }
 
         priceValue = "$" + priceValue.toFixed(2);
@@ -339,4 +341,3 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
     global.$dc = dc;
 
 })(window);
-
